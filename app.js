cf89a3538649f6b166d65a372c6a10d0b3417d97
OmegaRed767 <omegared767@gmail.com>
2023-08-24 14:35:25 +0530

    cors-error-fix

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ mongoose.connect(`mongodb+srv://raakesh210:${process.env.pass}@cluster0.6yg6iek.
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log('Connected!'));
-app.use(cors)
+    .then(() => console.log('Connected!')).catch(err => console.log("err", err))
+
+app.use(cors())
+app.use((req, res, next) => {
+    res.status(200).json({
+        msg: "hello server"
+    })
+})
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use("/product", productRoutes)
